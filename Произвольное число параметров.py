59b68f73c1987d346563ae3bfb19db97c42a16c9
Whale-99 <168347567+Whale-99@users.noreply.github.com>
2024-08-12 19:49:08 +0300

    Add files via upload

---
+++ Произвольное число параметров.py
@@ -0,0 +1,12 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+
+    for word in other_words:
+        if root_word in word or word in root_word:
+            same_words.append(word)
+
+    return same_words
+
+
+result = single_root_words("cat", "catalog", "dog", "catch", "caterpillar", "car", "catnip")
+print(result)
