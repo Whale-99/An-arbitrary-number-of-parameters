5d07f1cbe70272a3c5ef8d2630383b92e205871b
Whale-99 <168347567+Whale-99@users.noreply.github.com>
2024-10-19 00:39:12 +0300

    Update Произвольное число параметров.py

--- Произвольное число параметров.py
+++ Произвольное число параметров.py
@@ -1,12 +1,9 @@
 def single_root_words(root_word, *other_words):
     same_words = []
+    root_word = root_word.lower()
 
     for word in other_words:
-        if root_word in word or word in root_word:
+        if root_word in word.lower() or word.lower() in root_word:
             same_words.append(word)
 
     return same_words
-
-
-result = single_root_words("cat", "catalog", "dog", "catch", "caterpillar", "car", "catnip")
-print(result)
